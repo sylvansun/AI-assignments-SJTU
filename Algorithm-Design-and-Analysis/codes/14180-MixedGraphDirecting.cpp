54938b43d211ac159929c7aa9d09426ff43a9e26
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-03-13 15:46:31 +0800

    Create 14180-MixedGraphDirecting.cpp

---
+++ Algorithm-Design-and-Analysis/codes/14180-MixedGraphDirecting.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+
+#define MAX_N 100005
+
+int degree[MAX_N];//the in-degree of the n vertices
+int order[MAX_N];//the topological order after the directed edges are all added
+std::queue<int> edges[MAX_N];//edges[i] contains all directed edges from vertex i
+std::queue<int> deg0;//a dynamic queue contain all vertices with degree 0
+int currod;//current order in the topological sorting process
+
+int main()
+{
+	int n, p1, p2;
+	std::cin >> n >> p1 >> p2;
+	int a, b;//denote the edges in loop
+	for (int i = 0;i < p1;i++) {
+		std::cin >> a >> b;
+		edges[a].push(b);
+		degree[b]++;
+	}
+	for (int i = 1;i <= n;++i) {
+		if (!degree[i]) {
+			deg0.push(i);
+			order[i] = ++currod;
+		}
+	}
+	int currver;//current vertex 
+	int nextver;//next vertex
+	while (!deg0.empty()) {
+		currver = deg0.front();
+		deg0.pop();
+		while (!edges[currver].empty()) {//consider all directed edges from vertex currver
+			nextver = edges[currver].front();
+			edges[currver].pop();
+			degree[nextver]--;
+			if (!degree[nextver]) {//becomes a source
+				deg0.push(nextver);
+				order[nextver] = ++currod;
+			}
+		}
+	}
+	for (int i = 0;i < p2;++i) {
+		std::cin >> a >> b;
+		if (order[a] < order[b]) {
+			std::cout << a << " " << b << std::endl;
+		}
+		else {
+			std::cout << b << " " << a << std::endl;
+		}
+	}
+	return 0;
+}
