6711bc86d0c995e34945d0270847ba5a1b8c4bec
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-03-14 10:19:19 +0800

    Update 14180-MixedGraphDirecting.cpp
    a pretty simple one

--- Algorithm-Design-and-Analysis/codes/14180-MixedGraphDirecting.cpp
+++ Algorithm-Design-and-Analysis/codes/14180-MixedGraphDirecting.cpp
@@ -8,23 +8,21 @@ int order[MAX_N];//the topological order after the directed edges are all added
 std::queue<int> edges[MAX_N];//edges[i] contains all directed edges from vertex i
 std::queue<int> deg0;//a dynamic queue contain all vertices with degree 0
 int currod;//current order in the topological sorting process
+int n, p1, p2;
+
+void read_input();
+void show_output();
+void partial_tpsort();
 
 int main()
 {
-	int n, p1, p2;
-	std::cin >> n >> p1 >> p2;
-	int a, b;//denote the edges in loop
-	for (int i = 0;i < p1;i++) {
-		std::cin >> a >> b;
-		edges[a].push(b);
-		degree[b]++;
-	}
-	for (int i = 1;i <= n;++i) {
-		if (!degree[i]) {
-			deg0.push(i);
-			order[i] = ++currod;
-		}
-	}
+	read_input();
+	partial_tpsort();
+	show_output();
+	return 0;
+}
+
+void partial_tpsort() {
 	int currver;//current vertex 
 	int nextver;//next vertex
 	while (!deg0.empty()) {
@@ -40,6 +38,25 @@ int main()
 			}
 		}
 	}
+}
+
+void read_input() {
+	std::cin >> n >> p1 >> p2;
+	int a, b;//denote the edges in loop
+	for (int i = 0;i < p1;i++) {
+		std::cin >> a >> b;
+		edges[a].push(b);
+		degree[b]++;
+	}
+	for (int i = 1;i <= n;++i) {
+		if (!degree[i]) {
+			deg0.push(i);
+			order[i] = ++currod;
+		}
+	}
+}
+void show_output() {
+	int a, b;
 	for (int i = 0;i < p2;++i) {
 		std::cin >> a >> b;
 		if (order[a] < order[b]) {
@@ -49,5 +66,4 @@ int main()
 			std::cout << b << " " << a << std::endl;
 		}
 	}
-	return 0;
 }
