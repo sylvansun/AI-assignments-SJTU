1c15ec307d32bdfa80e78d9b5e26423c4490bdf1
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-04-21 11:33:27 +0800

    Update 14387-LongestIncreasingSequence.cpp
    Add notes and improve the efficiency of function solve

--- Algorithm-Design-and-Analysis/codes/14387-LongestIncreasingSequence.cpp
+++ Algorithm-Design-and-Analysis/codes/14387-LongestIncreasingSequence.cpp
@@ -15,7 +15,7 @@ struct Node{
     Node(){v=0;pre=NULL;}
     Node(int a, Node* p):v(a),pre(p){}
 };
-Node inf_node(INF,NULL);
+Node inf_node(INF,NULL);//use this to initialize the lis array
 
 Node a[MAX_N];//original data
 int n;//length of sequence
@@ -25,22 +25,14 @@ void read_input();
 int solve();
 int binary_search(int l,int r,int val);//search on array lis to find element less or equal to
 void show_result(Node* p);
-void show_state();
 
 int main(int argc, const char * argv[]) {
     read_input();
-    int length=solve();
-    Node* end=lis[length];
+    Node* end=lis[solve()];
     show_result(end);
     return 0;
 }
 
-void show_state(){
-    for(int i=1;i<=n;++i){
-        std::cout<<lis[i]->v<<" ";
-    }
-    std::cout<<std::endl;
-}
 void show_result(Node* p){
     if(p==NULL){
         return;
@@ -76,31 +68,27 @@ void read_input(){
     for(int i=0;i<n;++i){
         std::cin>>tmp;
         a[i]=Node(tmp,NULL);
+    }
+    for(int i=0;i<=n+1;++i){
         lis[i]=&inf_node;
     }
-    lis[n]=&inf_node;
-    lis[n+1]=&inf_node;
 }
 
 
 int solve(){
-    for(int i=0;i<n;++i){
+    int result=0;
+    for(int i=0;i<n;++i){//update the lis array to maintain the optimal LISequence at each iteration
         int val=a[i].v;
-        int pos=binary_search(1, i+1, val);
-        if(pos==1){
+        int pos=binary_search(1, i+1, val);//use binary search to find the position of current value
+        if(pos>result){//this logic update the endpoint of the LISequence
+            result=pos;
+        }
+        if(pos==1){//pos=1 means the LISequence ending at a[i] is a singleton
             lis[pos]=&a[i];
         }
         else{
-            a[i].pre=lis[pos-1];
-            lis[pos]=&a[i];
-        }
-        //show_state();
-    }
-    int result=0;
-    for(int i=1;i<=n+1;++i){
-        if(lis[i]->v==INF){
-            result=i-1;
-            break;
+            a[i].pre=lis[pos-1];//first let the node remember its previous node
+            lis[pos]=&a[i];//then update the lis array
         }
     }
     std::cout<<result<<std::endl;
