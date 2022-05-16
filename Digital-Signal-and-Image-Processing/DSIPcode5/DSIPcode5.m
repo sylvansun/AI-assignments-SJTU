ea53d60ba3e0adad07db9c843909a39031a63993
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-05-16 22:16:05 +0800

    Update DSIPcode5.m

--- Digital-Signal-and-Image-Processing/DSIPcode5/DSIPcode5.m
+++ Digital-Signal-and-Image-Processing/DSIPcode5/DSIPcode5.m
@@ -7,23 +7,29 @@ max = uint8(255*ones(900,1440));
 min = uint8(zeros(900,1440));
 
 rng default; 
-norm = normrnd(10,1,1000,1);
+norm = normrnd(40,2,1000,1);
 exp = exprnd(1,1000,1);
+uni = unifrnd(40,100,1000,1);
 hnorm = histogram(norm,64).Values;
 hexp = histogram(exp,64).Values;
+huni = histogram(uni,64).Values;
+
 
 
 q1 = histeq(r);
 q2exp = histeq(r,hexp);
 q2norm = histeq(r,hnorm);
-subplot(421),imshow(r),title('channel R');
-subplot(422),imhist(r),title('channel R histogram');
-subplot(423),imshow(q1),title('channel R equalization');
-subplot(424),imhist(q1),title('histogram after equalization');
-subplot(425),imshow(q2exp),title('channel R exponential match');
-subplot(426),imhist(q2exp),title('histogram after exponential match');
-subplot(427),imshow(q2norm),title('channel R normal match');
-subplot(428),imhist(q2norm),title('histogram after normal match');
+q3 = histeq(r,huni);
+subplot(521),imshow(r),title('Original Image channel R');
+subplot(522),imhist(r),title('channel R histogram');
+subplot(523),imshow(q1),title('Question1 channel R equalization');
+subplot(524),imhist(q1),title('histogram after equalization');
+subplot(525),imshow(q2exp),title('Question2 channel R exponential match');
+subplot(526),imhist(q2exp),title('histogram after exponential match');
+subplot(527),imshow(q2norm),title('Question2 channel R normal match');
+subplot(528),imhist(q2norm),title('histogram after normal match');
+subplot(529),imshow(q3),title('Question3 channel R uniform match');
+subplot(5,2,10),imhist(q3),title('histogram after uniform match');
 
 req = histeq(r);
 geq = histeq(g);
@@ -34,9 +40,11 @@ geq_norm = histeq(g,hexp);
 beq_norm = histeq(b,hexp);
 q5 = cat(3,req_norm,geq_norm,beq_norm);
 
-%subplot(311),imshow(orig);
-%subplot(312),imshow(q4);
-%subplot(313),imshow(q5);
+
+subplot(131),imshow(orig),title('Original Image');
+subplot(132),imshow(q4),title('Q4');
+subplot(133),imshow(q5),title('Q5');
+
 
 
 
